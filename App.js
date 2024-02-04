a18499bc4be907afc43ab71ee0842b0997c9efc8
Eliav Menachi <eliav.menachi@gmail.com>
2024-02-04 16:33:41 +0200

    add jest

--- App.js
+++ App.js
@@ -6,17 +6,19 @@ const studentRoute = require("./routes/student_route");
 const postRoute = require("./routes/post_route");
 const bodyParser = require("body-parser");
 
-mongoose.connect(process.env.DATABASE_URL);
-const db = mongoose.connection;
-db.on("error", (err) => console.log(err));
-db.once("open", () => console.log("Database connected"));
+const initApp = () => {
+  const promise = new Promise(async (resolve) => {
+    const db = mongoose.connection;
+    db.on("error", (err) => console.log(err));
+    db.once("open", () => console.log("Database connected"));
+    await mongoose.connect(process.env.DATABASE_URL);
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use("/student", studentRoute);
+    app.use("/post", postRoute);
+    resolve(app);
+  });
+  return promise;
+};
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use("/student", studentRoute);
-app.use("/post", postRoute);
-
-app.listen(process.env.PORT, () => {
-  console.log(`Example app listening at http://localhost:${process.env.PORT}`);
-});
+module.exports = initApp;
